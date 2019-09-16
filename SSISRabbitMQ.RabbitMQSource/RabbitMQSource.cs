102cbe6fc5dd4d91a2e17e5aa6126a8474ab8dae
Michael Burgess <mburgess@dedata.com.au>
2019-09-16 11:37:53 +1000

    Update to RabbitMQ 5 and SQL Server 2017

--- SSISRabbitMQ.RabbitMQSource/RabbitMQSource.cs
+++ SSISRabbitMQ.RabbitMQSource/RabbitMQSource.cs
@@ -1,179 +1,149 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.SqlServer.Dts.Pipeline;
+using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
 using Microsoft.SqlServer.Dts.Runtime;
 using Microsoft.SqlServer.Dts.Runtime.Wrapper;
-using Microsoft.SqlServer.Dts.Pipeline;
-using Microsoft.SqlServer.Dts.Pipeline.Wrapper;
 using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
+// https://kzhendev.wordpress.com/2013/08/13/part-3-building-a-custom-source-component/
 namespace SSISRabbitMQ.RabbitMQSource
 {
-  [DtsPipelineComponent(IconResource = "SSISRabbitMQ.RabbitMQSource.Rabbit.ico",
-    DisplayName = "RabbitMQ Source",
-    ComponentType = ComponentType.SourceAdapter,
-    Description = "Connection source for RabbitMQ",
-    UITypeName = "SSISRabbitMQ.RabbitMQSource.RabbitMQSourceUI, SSISRabbitMQ.RabbitMQSource, Version=1.0.0.0, Culture=neutral, PublicKeyToken=ac1c316408dd3955")]
-  public class RabbitMQSource : PipelineComponent
-  {
-    private IConnection rabbitConnection;
-    private IModel consumerChannel;
-    private QueueingBasicConsumer queueConsumer;
-
-    private RabbitMQConnectionManager.RabbitMQConnectionManager rabbitMqConnectionManager;
-
-    private string queueName;
-    private string consumerTag;
-
-    public override void ProvideComponentProperties()
+    [DtsPipelineComponent(IconResource = "SSISRabbitMQ.RabbitMQSource.Rabbit.ico",
+        DisplayName = "RabbitMQ Source",
+        ComponentType = ComponentType.SourceAdapter,
+        Description = "Connection source for RabbitMQ",
+        UITypeName = "SSISRabbitMQ.RabbitMQSource.RabbitMQSourceUI, SSISRabbitMQ.RabbitMQSource, Version=1.0.0.0, Culture=neutral, PublicKeyToken=ac1c316408dd3955")]
+    public class RabbitMQSource : PipelineComponent
     {
-      // Reset the component.
-      base.RemoveAllInputsOutputsAndCustomProperties();
-      ComponentMetaData.RuntimeConnectionCollection.RemoveAll();
+        private IConnection rabbitConnection;
+        private IModel consumerChannel;
 
-      IDTSOutput100 output = ComponentMetaData.OutputCollection.New();
-      output.Name = "Output";
+        private RabbitMQConnectionManager.RabbitMQConnectionManager rabbitMqConnectionManager;
 
-      IDTSCustomProperty100 queueName = ComponentMetaData.CustomPropertyCollection.New();
-      queueName.Name = "QueueName";
-      queueName.Description = "The name of the RabbitMQ queue to read messages from";
+        private string queueName;
 
-      IDTSRuntimeConnection100 connection = ComponentMetaData.RuntimeConnectionCollection.New();
-      connection.Name = "RabbitMQ";
-      connection.ConnectionManagerID = "RabbitMQ";
-
-      CreateColumns();
-    }
-
-    private void CreateColumns()
-    {
-      IDTSOutput100 output = ComponentMetaData.OutputCollection[0];
-
-      output.OutputColumnCollection.RemoveAll();
-      output.ExternalMetadataColumnCollection.RemoveAll();
-
-      IDTSOutputColumn100 column1 = output.OutputColumnCollection.New();
-      IDTSExternalMetadataColumn100 exColumn1 = output.ExternalMetadataColumnCollection.New();
-
-      IDTSOutputColumn100 column2 = output.OutputColumnCollection.New();
-      IDTSExternalMetadataColumn100 exColumn2 = output.ExternalMetadataColumnCollection.New();
-
-      column1.Name = "MessageContents";
-      column1.SetDataTypeProperties(DataType.DT_WSTR, 4000, 0, 0, 0);
-
-      column2.Name = "RoutingKey";
-      column2.SetDataTypeProperties(DataType.DT_WSTR, 100, 0, 0, 0);
-    }
-
-    public override DTSValidationStatus Validate()
-    {
-      bool cancel;
-      string qName = ComponentMetaData.CustomPropertyCollection["QueueName"].Value;
-
-      if (string.IsNullOrWhiteSpace(qName))
-      {
-        //Validate that the QueueName property is set
-        ComponentMetaData.FireError(0, ComponentMetaData.Name, "The QueueName property must be set", "", 0, out cancel);
-        return DTSValidationStatus.VS_ISBROKEN;
-      }
-
-      return base.Validate();
-    }
-
-    public override void AcquireConnections(object transaction)
-    {
-      if (ComponentMetaData.RuntimeConnectionCollection[0].ConnectionManager != null)
-      {
-        ConnectionManager connectionManager = Microsoft.SqlServer.Dts.Runtime.DtsConvert.GetWrapper(
-          ComponentMetaData.RuntimeConnectionCollection[0].ConnectionManager);
-
-        this.rabbitMqConnectionManager = connectionManager.InnerObject as RabbitMQConnectionManager.RabbitMQConnectionManager;
-
-        if (this.rabbitMqConnectionManager == null)
-          throw new Exception("Couldn't get the RabbitMQ connection manager, ");
-
-        this.queueName = ComponentMetaData.CustomPropertyCollection["QueueName"].Value;
-
-        rabbitConnection = this.rabbitMqConnectionManager.AcquireConnection(transaction) as IConnection;
-      }
-    }
-
-    public override void ReleaseConnections()
-    {
-      if (rabbitMqConnectionManager != null)
-      {
-        this.rabbitMqConnectionManager.ReleaseConnection(rabbitConnection);
-      }
-    }
-
-    public override void PreExecute()
-    {
-      try
-      {
-        this.consumerChannel = rabbitConnection.CreateModel();
-        this.consumerChannel.QueueDeclare(queueName, true, false, false, null);
-        this.queueConsumer = new QueueingBasicConsumer(this.consumerChannel);
-        this.consumerTag = consumerChannel.BasicConsume(queueName, true, queueConsumer);
-      }
-      catch (Exception)
-      {
-        ReleaseConnections();
-        throw;
-      }
-    }
-
-    public override void PrimeOutput(int outputs, int[] outputIDs, PipelineBuffer[] buffers)
-    {
-      IDTSOutput100 output = ComponentMetaData.OutputCollection[0];
-      PipelineBuffer buffer = buffers[0];
-
-      BasicDeliverEventArgs message;
-      bool success;
-
-      while (queueConsumer.IsRunning)
-      {
-        try
+        public override void ProvideComponentProperties()
         {
-          success = queueConsumer.Queue.Dequeue(100, out message);
-        }
-        catch (Exception)
-        {
-          break;
+            // Reset the component.
+            base.RemoveAllInputsOutputsAndCustomProperties();
+            ComponentMetaData.RuntimeConnectionCollection.RemoveAll();
+
+            var output = ComponentMetaData.OutputCollection.New();
+            output.Name = "Output";
+
+            var queueName = ComponentMetaData.CustomPropertyCollection.New();
+            queueName.Name = "QueueName";
+            queueName.Description = "The name of the RabbitMQ queue to read messages from";
+
+            var connection = ComponentMetaData.RuntimeConnectionCollection.New();
+            connection.Name = "RabbitMQ";
+            connection.ConnectionManagerID = "RabbitMQ";
+
+            CreateColumns();
         }
 
-        if (success)
+        private void CreateColumns()
         {
-          BasicDeliverEventArgs e = (BasicDeliverEventArgs)message;
+            var output = ComponentMetaData.OutputCollection[0];
 
-          var messageContent = System.Text.Encoding.UTF8.GetString(e.Body);
+            output.OutputColumnCollection.RemoveAll();
+            output.ExternalMetadataColumnCollection.RemoveAll();
 
-          buffer.AddRow();
-          buffer[0] = messageContent;
-          buffer[1] = e.RoutingKey;
+            var column1 = output.OutputColumnCollection.New();
+            var exColumn1 = output.ExternalMetadataColumnCollection.New();
+
+            var column2 = output.OutputColumnCollection.New();
+            var exColumn2 = output.ExternalMetadataColumnCollection.New();
+
+            column1.Name = "MessageContents";
+            column1.SetDataTypeProperties(DataType.DT_WSTR, 4000, 0, 0, 0);
+
+            column2.Name = "RoutingKey";
+            column2.SetDataTypeProperties(DataType.DT_WSTR, 100, 0, 0, 0);
         }
-        else
+
+        public override DTSValidationStatus Validate()
         {
-          break;
-        }
-      }
+            string qName = ComponentMetaData.CustomPropertyCollection["QueueName"].Value;
 
-      buffer.SetEndOfRowset();
+            if (string.IsNullOrWhiteSpace(qName))
+            {
+                //Validate that the QueueName property is set
+                ComponentMetaData.FireError(0, ComponentMetaData.Name, "The QueueName property must be set", "", 0, out bool cancel);
+                return DTSValidationStatus.VS_ISBROKEN;
+            }
+
+            return base.Validate();
+        }
+
+        public override void AcquireConnections(object transaction)
+        {
+            if (ComponentMetaData.RuntimeConnectionCollection[0].ConnectionManager != null)
+            {
+                ConnectionManager connectionManager = DtsConvert.GetWrapper(
+                  ComponentMetaData.RuntimeConnectionCollection[0].ConnectionManager);
+
+                this.rabbitMqConnectionManager = connectionManager.InnerObject as RabbitMQConnectionManager.RabbitMQConnectionManager;
+
+                if (this.rabbitMqConnectionManager == null)
+                    throw new Exception("Couldn't get the RabbitMQ connection manager, ");
+
+                this.queueName = ComponentMetaData.CustomPropertyCollection["QueueName"].Value;
+
+                rabbitConnection = this.rabbitMqConnectionManager.AcquireConnection(transaction) as IConnection;
+            }
+        }
+
+        public override void ReleaseConnections()
+        {
+            if (rabbitMqConnectionManager != null)
+            {
+                this.rabbitMqConnectionManager.ReleaseConnection(rabbitConnection);
+            }
+        }
+
+        public override void PreExecute()
+        {
+            try
+            {
+                this.consumerChannel = rabbitConnection.CreateModel();
+                this.consumerChannel.QueueDeclare(queueName, true, false, false, null);
+            }
+            catch (Exception)
+            {
+                ReleaseConnections();
+                throw;
+            }
+        }
+
+        public override void PrimeOutput(int outputs, int[] outputIDs, PipelineBuffer[] buffers)
+        {
+            IDTSOutput100 output = ComponentMetaData.OutputCollection[0];
+            PipelineBuffer buffer = buffers[0];
+
+            var result = consumerChannel.BasicGet(queueName, true);
+            while (result != null)
+            {
+                var messageContent = Encoding.UTF8.GetString(result.Body);
+
+                buffer.AddRow();
+                buffer[0] = messageContent;
+                buffer[1] = result.RoutingKey;
+
+                result = consumerChannel.BasicGet(queueName, true);
+            }
+
+            buffer.SetEndOfRowset();
+        }
+
+        public override void Cleanup()
+        {
+            if (consumerChannel.IsOpen)
+            {
+                consumerChannel.Close();
+            }
+            base.Cleanup();
+        }
     }
-
-    public override void Cleanup()
-    {
-      if (consumerChannel.IsOpen)
-      {
-        if (queueConsumer.IsRunning)
-        {
-          consumerChannel.BasicCancel(consumerTag);
-        }
-        consumerChannel.Close();
-      }
-      base.Cleanup();
-    }
-  }
 }
